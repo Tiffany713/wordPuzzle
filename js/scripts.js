7ab5a7e7ffd24907835a9cecf1400082627b38ed
elliot jordan and Tiffany Nguyen <elliot@>
2020-01-22 15:47:59 -0800

    added .length property function

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,58 @@
-$(document).ready(function() {
-    $("form").submit(function() {
-        var userSentence = $("input#userSentence").val();
-        console.log(userSentence);
+
+$(document).ready(function(){
+    $("form").submit(function(event){
+      event.preventDefault();
+
+      $("#affect-responses").show();
+      $("input:checkbox[name=affect]:checked").each(function(){
+        var stressAffect = $(this).val();
+        $('#affect-responses').append(stressAffect + "<br>");
+        // console.log(stressAffect);
         
-        var breakUp = userSentence.split("");
-        console.log(breakUp);
+      });
+      $("#symptoms-responses").show();
+      $("input:checkbox[name=symptoms]:checked").each(function(){
+        var stressSymptoms = $(this).val();
+        $('#symptoms-responses').append(stressSymptoms + "<br>");
+        // console.log(stressSymptoms);
+      });
 
-        function replace(array) {
-            // var newString = "";
-            for (var i=0; i < array.length; i++){
-                if (["a", "i", "u", "e", "o"].includes(array[i])) {
-                    array[i] = "-";  
-                }        
-            }
-            return array;
-        }
+      $("#solution-responses").show();
+      $("input:checkbox[name=solution]:checked").each(function(){
+        var stressSolution = $(this).val();
+        $('#solution-responses').append(stressSolution + "<br>");
+        // console.log(stressSolution);
+      });
+      
+      var countChecked1 = function() {
+        var n = $( "input:checkbox[name=affect]:checked" ).length;
+        $("#links").text(n);
+      };
+      countChecked1();
 
-        var newSent = replace(breakUp).join("");
-        console.log(newSent);
-        // $("#output").text(newSent);
+      var countChecked2 = function() {
+        var n = $( "input:checkbox[name=symptoms]:checked" ).length;
+        $("#links2").text(n);
+      };
+      countChecked2();
 
-        event.preventDefault();
-    });  
-});
+      var countChecked3 = function() {
+        var n = $( "input:checkbox[name=solution]:checked" ).length;
+        $("#links3").text(n);
+      };
+      countChecked3();
+
+
+      $("#links").show();
+      $("#links2").show();
+      $("#links3").show();
+      
+      $('form').hide();
+    });
+  });
+  
+
+
+        // for (var i=0; i < Array.length; i++) {
+        //   if (["> 3"])
+        // }
